52624b47e65820178c525ce80ef117559a5bb565
LAPTOP-8IAMKR59\sklsh <sarah.shoup@okstate.edu>
2021-04-15 12:15:38 -0500

    added text to Portal prefab for debugging use in AR, fixed camera collider comparison in moveThroughDoor!

--- AR doorway/Assets/Scripts/moveThroughDoor.cs
+++ AR doorway/Assets/Scripts/moveThroughDoor.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.UI;
 
 public class moveThroughDoor : MonoBehaviour
 {
 
     public Material[] materials;
+    public Text myText;
 
     void Start()
     {
+        //set shaders displaying as camera is outside of other world at start
         foreach (var mat in materials)
         {
             mat.SetInt("_StencilTest", (int)CompareFunction.Equal);
@@ -18,21 +21,26 @@ public class moveThroughDoor : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.name != "Main Camera")
+        //compare "other" collider (what is hitting the doorWindow) with the main camera's collider
+        Camera myCamera = Camera.main;
+        if (myCamera.GetComponent<Collider>() != other){
             return;
-
+        }
+        //myText.text = "checkpoint reached";
 
         //Outside of other world
-        if(transform.position.z > other.transform.position.z)
+        if (transform.position.z > other.transform.position.z)
         {
-            Debug.Log("Outside of other world");
-            foreach(var mat in materials)
+            myText.text = "outside of other world";
+            //Debug.Log("Outside of other world");
+            foreach (var mat in materials)
             {
                 mat.SetInt("_StencilTest", (int)CompareFunction.Equal);
             }
         }//Inside other world
         else {
-            Debug.Log("Inside other world");
+            myText.text = "inside other world";
+            //Debug.Log("Inside other world");
             foreach(var mat in materials)
             {
                 mat.SetInt("_StencilTest", (int)CompareFunction.NotEqual);
--- README.md
+++ README.md
@@ -5,4 +5,4 @@ App 2 [WIP]: Places an AR doorway using a reticle. This doorway can then be walk
 
   -Placeable AR door: Placement is going okay, and can see the interior through the door. However, when you walk through the door it doesn't allow you to see the interior, but rather swaps and you can see the "interior" objects peeking out the sides when looking back through the doorway (I believe it's just not swapping the shader properly when you walk through?)
   
-  -AR doorway: Likewise is okay with placement and looking through the doorway. However, also currently doesn't register that you're "inside the other world" when you cross through the doorway. It almost appears as though the moveThroughDoor script is never activating?
+  -AR doorway: Likewise is okay with placement and looking through the doorway. This version however does recognize when you are in/out of the other world, and displays just a cube inside the traversable interior.
